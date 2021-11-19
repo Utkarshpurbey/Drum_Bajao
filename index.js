e27372c2186d4c5f7fe2f59f5b575a9051163e4a
Utkarshpurbey <77148512+Utkarshpurbey@users.noreply.github.com>
2021-11-19 20:09:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var array=document.querySelectorAll("button");
+function soundPlay( char)
+{
+  switch (char) {
+    case "w":
+    var audio= new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case "a":
+    var audio= new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case "s":
+    var audio= new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case "d":
+    var audio= new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    case "j":
+    var audio= new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case "k":
+    var audio= new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+
+    case "l":
+    var audio= new Audio("sounds/kick-bass.mp3")
+    audio.play();
+    break;
+
+    default:
+
+  }
+}
+for(var i=0;i< array.length;i++)
+
+{
+  document.querySelectorAll("button")[i].addEventListener("click",function (){
+    var ButtonPressed= this.textContent;
+    soundPlay(ButtonPressed);
+});
+document.addEventListener("keypress",function(event)
+{
+  soundPlay(event.key);
+} );
+}
